dbb1c4ae37237bbc01901d03153d8552a38ed9e6
heehminh <mink141416@gmail.com>
2023-03-29 12:58:54 +0900

    week1: airbnb 클론코딩 (html, css)

---
+++ 안나-김민희/01-airbnb/data/types.js
@@ -0,0 +1,204 @@
+const types = [
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망1",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+    type_description: "B&B",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+    type_description: "개인실",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+    type_description: "인기 급상승",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망1",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+    type_description: "B&B",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+    type_description: "개인실",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+    type_description: "인기 급상승",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망1",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg",
+    type_description: "B&B",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/eb7ba4c0-ea38-4cbb-9db6-bdcc8baad585.jpg",
+    type_description: "개인실",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg",
+    type_description: "인기 급상승",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/51f5cf64-5821-400c-8033-8a10c7787d69.jpg",
+    type_description: "한옥",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
+    type_description: "최고의 전망",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
+    type_description: "한적한 시골",
+  },
+  {
+    type_imgSrc:
+      "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
+    type_description: "돔하우스",
+  },
+];
+
+export default types;
