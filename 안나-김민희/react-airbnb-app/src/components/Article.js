d96670f207ea2fd93e298ba7ec1ca638e3440c4b
heehminh <mink141416@gmail.com>
2023-04-09 13:26:37 +0900

    Feat: like onClick

--- 안나-김민희/react-airbnb-app/src/components/Article.js
+++ 안나-김민희/react-airbnb-app/src/components/Article.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import rooms from "../data/rooms";
+import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 
 const Article = () => {
+  const [likedRooms, setLikedRooms] = useState([]);
+
+  const toggleLike = (index) => {
+    const newLikedRooms = [...likedRooms];
+    newLikedRooms[index] = !newLikedRooms[index];
+    setLikedRooms(newLikedRooms);
+  };
+
   return (
     <article className="room">
       {rooms.map((room, index) => (
         <div className="room__item" key={index}>
           <div className="room__item__imgBox">
             <img className="imgBox" src={room.img} alt="imgBox" />
-            <img
-              className="imgBox__like"
-              src="https://pixlok.com/wp-content/uploads/2021/12/Instagram-Like-Icon-03brf3.png"
-              alt="imgBoxLike"
-            />
+            {likedRooms[index] ? (
+              <HeartFilled
+                className="room__like"
+                onClick={() => toggleLike(index)}
+              />
+            ) : (
+              <HeartOutlined
+                className="room__unlike"
+                onClick={() => toggleLike(index)}
+              />
+            )}
           </div>
           <div className="room__item__textBox">
             <div className="textBox__1">
