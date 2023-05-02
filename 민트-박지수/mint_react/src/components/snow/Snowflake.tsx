382f04e20ce4eab99dbe80953c3702917446dcb1
박지수 <jisupark2000@gmail.com>
2023-05-02 17:26:10 +0900

    [FEAT] page404 눈내리는 효과 완성

--- 민트-박지수/mint_react/src/components/snow/Snowflake.tsx
+++ 민트-박지수/mint_react/src/components/snow/Snowflake.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './Snowflake.module.css';
+import uuid from 'react-uuid';
 
-const MIN_DURATION = 3; // 애니메이션 최소 지속 시간
+const MIN_DURATION = 0.3; // 애니메이션 최소 지속 시간
 
 interface SnowflakeProps {
-  makeSnowflake: () => void;
-  removeSnowflake: () => void;
-  n: number;
+  setSnowList: React.Dispatch<React.SetStateAction<string[]>>;
+  _key: string;
 }
-const Snowflake: React.FC<SnowflakeProps> = ({ makeSnowflake, removeSnowflake, n }) => {
-  console.log('생성');
+const Snowflake: React.FC<SnowflakeProps> = ({ setSnowList, _key }) => {
   const [animationProps, setAnimationProps] = useState({
     left: `${Math.random() * window.screen.width}px`,
     animationDuration: Math.random() * 20 + MIN_DURATION,
@@ -17,14 +16,25 @@ const Snowflake: React.FC<SnowflakeProps> = ({ makeSnowflake, removeSnowflake, n
     opacity: Math.random(),
   });
 
-  setTimeout(() => {
-    removeSnowflake();
-    console.log(n);
-
-    // makeSnowflake();
-  }, (animationProps.animationDuration + animationProps.animationDelay) * 1000);
-
   const { left, animationDuration, animationDelay, opacity } = animationProps;
+
+  const makeSnowflake = useCallback(() => {
+    setSnowList((prev) => [...prev, uuid()]);
+  }, [setSnowList]);
+  const removeSnowflake = useCallback(
+    (key: string) => {
+      setSnowList((prev) => prev.filter((snow) => snow !== key));
+      makeSnowflake();
+    },
+    [makeSnowflake, setSnowList]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      removeSnowflake(_key);
+    }, (animationDuration + animationDelay) * 1000);
+  }, [removeSnowflake, _key, animationDuration, animationDelay]);
+
   return (
     <div
       className={styles.snowflake}
