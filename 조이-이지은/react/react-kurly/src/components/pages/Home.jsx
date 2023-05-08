2e848c0329fbecd2ea25fe23bd767cbe60b6fe21
jieun <najee0610@naver.com>
2023-05-08 13:30:31 +0900

    Feat: src 이미지 컴포넌트화

--- 조이-이지은/react/react-kurly/src/components/pages/Home.jsx
+++ 조이-이지은/react/react-kurly/src/components/pages/Home.jsx
@@ -4,6 +4,18 @@ import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 
+import { MainCart } from "../images/imgSrc";
+import { Egg } from "../images/imgSrc";
+import { Choco } from "../images/imgSrc";
+import { Ddeok } from "../images/imgSrc";
+import { Plate } from "../images/imgSrc";
+import { FooterBanner } from "../images/imgSrc";
+import { Slide1 } from "../images/imgSrc";
+import { Slide2 } from "../images/imgSrc";
+import { Slide3 } from "../images/imgSrc";
+import { Slide4 } from "../images/imgSrc";
+import { Slide5 } from "../images/imgSrc";
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
@@ -28,42 +40,27 @@ function Home() {
             <ul>
               <SwiperSlide>
                 <li>
-                  <img
-                    src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/5b4e9985-9b15-4852-ae89-ffbed676c7c7.jpg"
-                    alt="price"
-                  />
+                  <img src={Slide1} alt="price" />
                 </li>
               </SwiperSlide>
               <SwiperSlide>
                 <li>
-                  <img
-                    src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/f70905f4-7ef0-4b52-98a3-204c85a3d1ca.png"
-                    alt="price"
-                  />
+                  <img src={Slide2} alt="price" />
                 </li>
               </SwiperSlide>
               <SwiperSlide>
                 <li>
-                  <img
-                    src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/6dcb9a88-452e-4545-a452-0b9855112f7f.jpg"
-                    alt="price"
-                  />
+                  <img src={Slide3} alt="price" />
                 </li>
               </SwiperSlide>
               <SwiperSlide>
                 <li>
-                  <img
-                    src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/cbd9a5cc-a0f8-4bd7-ba85-92ede8fc82c9.jpg"
-                    alt="price"
-                  />
+                  <img src={Slide4} alt="price" />
                 </li>
               </SwiperSlide>
               <SwiperSlide>
                 <li>
-                  <img
-                    src="https://product-image.kurly.com/cdn-cgi/image/quality=85/banner/main/pc/img/f70905f4-7ef0-4b52-98a3-204c85a3d1ca.png"
-                    alt="price"
-                  />
+                  <img src={Slide5} alt="price" />
                 </li>
               </SwiperSlide>
             </ul>
@@ -77,11 +74,8 @@ function Home() {
           <div className="main-2-wrap">
             <div className="main-2-img">
               <Link to={"/egg"}>
-                <img
-                  src="https://img-cf.kurly.com/shop/data/goods/1637147626889l0.jpg"
-                  width="230px"
-                  alt="egg"
-                />
+                <img src={Egg} width="230px" alt="egg" />
+                <img src={MainCart} alt="main-cart" />
               </Link>
               <span className="main-2-name">
                 [KF365] 1+등급 무항생제 특란 20구
@@ -90,11 +84,7 @@ function Home() {
               <span className="main-2-re">후기 9,999+</span>
             </div>
             <div className="main-2-img">
-              <img
-                src="https://img-cf.kurly.com/shop/data/goods/1583817046139l0.jpg"
-                width="230px"
-                alt="choco"
-              />
+              <img src={Choco} width="230px" alt="choco" />
               <span className="main-2-name">
                 [그랜드벨지안] 초콜릿 트윅스 4종
               </span>
@@ -102,11 +92,7 @@ function Home() {
               <span className="main-2-re">후기 999+</span>
             </div>
             <div className="main-2-img">
-              <img
-                src="https://img-cf.kurly.com/shop/data/goods/1589873216873l0.jpg"
-                width="230px"
-                alt="plate"
-              />
+              <img src={Plate} width="230px" alt="plate" />
               <span className="main-2-name">
                 [오덴세] 아틀리에 노드 세트 2종 (택1)
               </span>
@@ -114,11 +100,7 @@ function Home() {
               <span className="main-2-re">후기 46</span>
             </div>
             <div className="main-2-img">
-              <img
-                src="https://img-cf.kurly.com/shop/data/goods/1656567944707l0.jpg"
-                width="230px"
-                alt="ddeok"
-              />
+              <img src={Ddeok} width="230px" alt="ddeok" />
               <span className="main-2-name">
                 [떡편] 쌀케이크 세트 (커피+고구마)
               </span>
@@ -128,10 +110,7 @@ function Home() {
           </div>
         </section>
         <section className="main-3">
-          <img
-            src="https://product-image.kurly.com/banner/random-band/mobile/img/585a35ba-4d17-428d-ae78-750c5dd66189.jpg"
-            alt="footer-banner"
-          />
+          <img src={FooterBanner} alt="footer-banner" />
         </section>
       </main>
     </div>
