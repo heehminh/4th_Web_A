72f069163b981435321d7f357b28aa08c5bb8b43
heehminh <mink141416@gmail.com>
2023-05-09 13:31:14 +0900

    Feat: clcik type

--- 안나-김민희/react-airbnb-app/src/components/Type.js
+++ 안나-김민희/react-airbnb-app/src/components/Type.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import types from "../data/types";
 
 const Type = ({ setTypeIndex }) => {
+  const [activeTypeItemId, setActiveTypeItemId] = useState(null);
+
+  const handleTypeItemClick = (typeIdx) => {
+    setActiveTypeItemId(typeIdx);
+    setTypeIndex(typeIdx);
+  };
+
   return (
     <Wrapper>
       <TypeWrapper>
         {types.map((type, index) => (
-          <TypeItem key={index} onClick={() => setTypeIndex(type.type_idx)}>
+          <TypeItem
+            key={index}
+            className={activeTypeItemId === type.type_idx ? "active" : ""}
+            onClick={() => handleTypeItemClick(type.type_idx)}
+          >
             <TypeItemIcon src={type.type_imgSrc} alt="type" />
             <TypeItemDescription>{type.type_description}</TypeItemDescription>
           </TypeItem>
@@ -44,23 +55,51 @@ const TypeWrapper = styled.div`
   }
 `;
 
-const TypeItem = styled.div`
+const TypeItem = styled.button`
   width: 80px;
-  height: 70px;
+  height: 80px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  justify-content: flex-start;
   margin-right: 30px;
+  padding-bottom: 20px;
+  border: 2px solid white;
+  transition: border-bottom-color 0.2s ease-in-out;
+  background-color: white;
+  cursor: pointer;
+
+  &:hover {
+    border-bottom-color: gray;
+    cursor: pointer;
+  }
+
+  &.active {
+    border-bottom-color: black;
+  }
 `;
 
 const TypeItemIcon = styled.img`
   width: 30px;
+  opacity: 0.5;
+
+  ${TypeItem}:hover &,
+  ${TypeItem}.active & {
+    opacity: 1;
+  }
 `;
 
 const TypeItemDescription = styled.div`
-  font-size: 12px;
+  font-size: 14px;
+  font-weight: 600;
   margin-top: 20px;
+  color: gray;
+  transition: color 0.2s ease-in-out;
+
+  ${TypeItem}:hover &,
+  ${TypeItem}.active & {
+    color: black;
+  }
 `;
 
 const TypeFilter = styled.div`
