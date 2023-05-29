eb3d7c1b5e150c1530bc226277d63b4d88f4ec81
heehminh <mink141416@gmail.com>
2023-05-30 01:09:49 +0900

    Feat: useForm 적용+styled components

--- 안나-김민희/react-form-app/src/App.js
+++ 안나-김민희/react-form-app/src/App.js
@@ -1,20 +1,145 @@
 import React from "react";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const App = () => {
+  const { register, handleSubmit } = useForm();
   return (
-    <form>
-      <label htmlFor="name">학우님의 이름은 무엇인가요?</label>
-      <input id="name" type="text" name="name" />
-      <label htmlFor="stuNo">학번을 적어주세요.</label>
-      <input id="stuNo" type="text" name="stuNo" />
-      <label htmlFor="agree">
-        봉사 중 촬영한 사진에 대해서 이후 홍보물 등의 사용에 동의하시나요?
-      </label>
-      <input type="radio" name="agree" /> 예
-      <input type="radio" name="agree" /> 아니오
-      <button type="submit">제출하기</button>
-    </form>
+    <Wrapper>
+      <Box title>
+        <Title>행사 참석 여부</Title>
+        <Description>
+          행사장 주소: 서울시 강남구 가나다로 123, 12345 <br />
+          연락처 정보: (+82)000-0000 또는 abcd@gmail.com
+        </Description>
+      </Box>
+      <form onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
+        <Box>
+          <Label htmlFor="name">참석하는 사람들의 성함을 알려주세요.</Label>
+          <Input
+            text
+            id="name"
+            type="text"
+            name="name"
+            placeholder="내 답변"
+            {...register("name")}
+          />
+        </Box>
+        <Box>
+          <Label htmlFor="how">행사에 관해 어떻게 알게 되셨나요?</Label>
+          <RadioBox>
+            <Input type="radio" name="how" {...register("how")} /> 웹사이트
+          </RadioBox>
+          <RadioBox>
+            <Input type="radio" name="how" {...register("how")} /> 친구
+          </RadioBox>
+          <RadioBox>
+            <Input type="radio" name="how" {...register("how")} /> 뉴스레터
+          </RadioBox>
+          <RadioBox>
+            <Input type="radio" name="how" {...register("how")} /> 광고
+          </RadioBox>
+        </Box>
+        <Box>
+          <Label htmlFor="question">의견 또는 질문</Label>
+          <Input
+            text
+            id="question"
+            type="text"
+            name="question"
+            placeholder="내 답변"
+            {...register("question")}
+          />
+        </Box>
+        <BtnBox>
+          <SubmitBtn type="submit">제출</SubmitBtn>
+          <RemoveBtn>양식 지우기</RemoveBtn>
+        </BtnBox>
+      </form>
+    </Wrapper>
   );
 };
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 90vw;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  background-color: lightgray;
+  padding: 5vw;
+`;
+
+const Box = styled.div`
+  width: 90vw;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background-color: white;
+  border-radius: 10px;
+  border-top: ${(props) =>
+    props.title ? "2vw solid darkred" : "0px solid darkred"};
+  margin: 1vw 0;
+  padding: 1vw;
+`;
+
+const Title = styled.div`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 1vw;
+`;
+
+const Description = styled.div`
+  margin: 1vw;
+  font-size: 1rem;
+`;
+
+const Label = styled.label`
+  margin: 1vw;
+  font-size: 1rem;
+`;
+
+const Input = styled.input`
+  width: ${(props) => (props.text ? "86vw" : "auto")};
+  margin: 1vw;
+  font-size: 1rem;
+  border: none;
+  border-bottom: 1px solid lightgray;
+  padding: 1vw;
+`;
+
+const RadioBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const BtnBox = styled.div`
+  width: 92vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const SubmitBtn = styled.button`
+  width: 10vw;
+  height: 5vw;
+  background-color: darkred;
+  border: none;
+  color: white;
+  font-size: 1.3rem;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const RemoveBtn = styled.button`
+  width: 12vw;
+  height: 5vw;
+  background-color: transparent;
+  border: none;
+  color: darkred;
+  font-size: 1.3rem;
+  border-radius: 10px;
+  cursor: pointer;
+`;
